77a0a9b9e40409804b7aef16f609fc7d3db87429
fogesque <pavelmatviyuk@gmail.com>
2025-11-10 14:38:15 +0300

    Implement FlowConfig and FlowMode classes with builder pattern for flow configuration

--- doca-cpp/include/doca-cpp/flow/flow_config.hpp
+++ doca-cpp/include/doca-cpp/flow/flow_config.hpp
@@ -10,8 +10,31 @@
 #include "doca-cpp/core/error.hpp"
 #include "doca-cpp/core/types.hpp"
 
-namespace doca
+namespace doca::flow
 {
+
+enum class FlowMode {
+    virtualNetworkFunctionWithHardwareSteering,
+    embeddedSwitch,
+};
+
+std::string FlowModeToString(FlowMode mode);
+
+enum class FlowSharedResourceType {
+    sharedMeter = DOCA_FLOW_SHARED_RESOURCE_METER,
+    sharedCounter = DOCA_FLOW_SHARED_RESOURCE_COUNTER,
+    sharedReceiveSideScaling = DOCA_FLOW_SHARED_RESOURCE_RSS,
+    sharedMirror = DOCA_FLOW_SHARED_RESOURCE_MIRROR,
+    sharedPsp = DOCA_FLOW_SHARED_RESOURCE_PSP,  // TODO: What the fuck is PSP?
+    sharedEncapsulation = DOCA_FLOW_SHARED_RESOURCE_ENCAP,
+    sharedDecapsulation = DOCA_FLOW_SHARED_RESOURCE_DECAP,
+    sharedIpsecSecurityAssociation = DOCA_FLOW_SHARED_RESOURCE_IPSEC_SA,
+};
+
+// TODO: implement these wrappers
+class FlowTuneConfig;
+class FlowReceiveSideScalingConfig;
+class FlowDefinitions;
 
 struct FlowConfigDeleter {
     void operator()(doca_flow_cfg * cfg) const;
@@ -25,15 +48,24 @@ public:
     public:
         ~Builder();
 
-        Builder & SetPermissions();
-        Builder & SetMemoryRange();
-        Builder & SetMaxNumDevices();
-        Builder & SetUserData();
-        std::tuple<FlowConfig, error> Start();
+        // TODO: research config parameters meanings
+        Builder & SetTuneConfig(const FlowTuneConfig & tuneCfg);
+        Builder & SetPipeQueues(uint16_t pipeQueues);
+        Builder & SetNumberOfCounters(uint32_t countersNumber);
+        Builder & SetNumberOfMeters(uint32_t metersNumber);
+        Builder & SetNumberOfAccessControlListCollisions(uint32_t aclCollisionsNumber);
+        Builder & SetFlowMode(FlowMode mode);
+        Builder & SetNumberOfSharedResource(FlowSharedResourceType resourceType, uint32_t resourcesNumber);
+        Builder & SetQueueDepth(uint32_t queueDepth);
+        Builder & SetReceiveSideScalingKey(std::shared_ptr<uint8_t> rssKey, uint32_t rssKeyLength);
+        Builder & SetGlobalDefaultReceiveSideScalingConfig(const FlowReceiveSideScalingConfig & rssCfg);
+        Builder & SetDefinitions(const FlowDefinitions & definitions);
+
+        std::tuple<FlowConfig, error> Build();
 
     private:
         friend class FlowConfig;
-        explicit Builder(doca_flow_cfg * m);
+        explicit Builder(doca_flow_cfg * cfg);
 
         Builder(const Builder &) = delete;
         Builder & operator=(const Builder &) = delete;
@@ -60,4 +92,4 @@ private:
     std::unique_ptr<doca_flow_cfg, FlowConfigDeleter> flowConfig;
 };
 
-}  // namespace doca
+}  // namespace doca::flow
